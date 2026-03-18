719b74f9782319d810d06ebb0f8acba926528275
xxh2333 <2633681826@qq.com>
2026-03-18 21:02:34 +0800

    完成邮箱SMTP配置并验证邮件发送成功
    增加了一个test文件，作为不建立数据库来测试smtp，呃呃以及因为composer.json的冲突，删了重新下载了一遍呃呃呃。有些.env.example里面的东西队友要自己更改喵

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//测试邮箱配置是否生效
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/test-mail', function () {
+    Mail::raw('这是一封测试邮件', function ($message) {
+        $message->to('测试接收邮箱@xxx.com')->subject('Laravel 邮件测试');
+    });
+    return '邮件发送成功';
+});
