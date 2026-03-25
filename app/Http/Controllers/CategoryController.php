d2584f5fa9feed00eb0e11e27a11958762bd6a4e
Lizhiwei415221 <193952040@qq.com>
2026-03-25 20:23:45 +0800

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
+
 
 class CategoryController extends Controller
 {
